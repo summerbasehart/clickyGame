f9c8fd439a2566e076b6722c4c404dd7c5844844
unknown <summerbasehart@gmail.com>
2019-07-15 01:40:38 -0700

    Updates

---
+++ precache-manifest.8322ebb3d40af0f04d63c2a44c5f164e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52de1239c6bd5f6660a910001205b0e9",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "56142fa86d3d9f662add",
+    "url": "/clickyGame/static/css/main.cf011066.chunk.css"
+  },
+  {
+    "revision": "d673ad7196131c6000e8",
+    "url": "/clickyGame/static/js/2.c21362d1.chunk.js"
+  },
+  {
+    "revision": "56142fa86d3d9f662add",
+    "url": "/clickyGame/static/js/main.d4c10ac3.chunk.js"
+  },
+  {
+    "revision": "95f5ce002aae55a30c31",
+    "url": "/clickyGame/static/js/runtime~main.d5f62368.js"
+  }
+]);
